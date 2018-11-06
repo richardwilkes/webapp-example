741ba9f6d3b4cd4b430253537a71cc4962bf70a8
Richard Wilkes <wilkes@me.com>
2018-11-06 13:38:30 -0800

    Fix helper naming

--- build.sh
+++ build.sh
@@ -52,15 +52,15 @@ mkdir -p dist/$OS_TYPE
 case $OS_TYPE in
     darwin)
         TARGET_EXE="dist/$OS_TYPE/$APP_BUNDLE_DISPLAY_NAME.app/Contents/MacOS/$APP_NAME"
-        mkdir -p "dist/$OS_TYPE/$APP_BUNDLE_DISPLAY_NAME.app/Contents/Frameworks/$APP_BUNDLE_DISPLAY_NAME Helper.app/Contents/MacOS"
-        mkdir -p "dist/$OS_TYPE/$APP_BUNDLE_DISPLAY_NAME.app/Contents/Frameworks/$APP_BUNDLE_DISPLAY_NAME Helper.app/Contents/Frameworks"
+        mkdir -p "dist/$OS_TYPE/$APP_BUNDLE_DISPLAY_NAME.app/Contents/Frameworks/$APP_NAME Helper.app/Contents/MacOS"
+        mkdir -p "dist/$OS_TYPE/$APP_BUNDLE_DISPLAY_NAME.app/Contents/Frameworks/$APP_NAME Helper.app/Contents/Frameworks"
         cc -I cef $GOPATH/src/github.com/richardwilkes/webapp/helper/cef_helper.c \
             -F cef/Release -framework "Chromium Embedded Framework" \
-            -o "dist/$OS_TYPE/$APP_BUNDLE_DISPLAY_NAME.app/Contents/Frameworks/$APP_BUNDLE_DISPLAY_NAME Helper.app/Contents/MacOS/$APP_NAME Helper"
+            -o "dist/$OS_TYPE/$APP_BUNDLE_DISPLAY_NAME.app/Contents/Frameworks/$APP_NAME Helper.app/Contents/MacOS/$APP_NAME Helper"
         mkdir -p "dist/$OS_TYPE/$APP_BUNDLE_DISPLAY_NAME.app/Contents/MacOS"
         mkdir -p "dist/$OS_TYPE/$APP_BUNDLE_DISPLAY_NAME.app/Contents/Resources"
         cp -R "cef/Release/Chromium Embedded Framework.framework" "dist/$OS_TYPE/$APP_BUNDLE_DISPLAY_NAME.app/Contents/Frameworks/"
-        ln -s "../../../Chromium Embedded Framework.framework" "dist/$OS_TYPE/$APP_BUNDLE_DISPLAY_NAME.app/Contents/Frameworks/$APP_BUNDLE_DISPLAY_NAME Helper.app/Contents/Frameworks/Chromium Embedded Framework.framework"
+        ln -s "../../../Chromium Embedded Framework.framework" "dist/$OS_TYPE/$APP_BUNDLE_DISPLAY_NAME.app/Contents/Frameworks/$APP_NAME Helper.app/Contents/Frameworks/Chromium Embedded Framework.framework"
         cp AppIcon.icns "dist/$OS_TYPE/$APP_BUNDLE_DISPLAY_NAME.app/Contents/Resources/AppIcon.icns"
         cat > "dist/$OS_TYPE/$APP_BUNDLE_DISPLAY_NAME.app/Contents/Info.plist" << EOF
 <?xml version="1.0" encoding="UTF-8"?>
@@ -94,7 +94,7 @@ case $OS_TYPE in
 </dict>
 </plist>
 EOF
-        cat > "dist/$OS_TYPE/$APP_BUNDLE_DISPLAY_NAME.app/Contents/Frameworks/$APP_BUNDLE_DISPLAY_NAME Helper.app/Contents/Info.plist" << EOF
+        cat > "dist/$OS_TYPE/$APP_BUNDLE_DISPLAY_NAME.app/Contents/Frameworks/$APP_NAME Helper.app/Contents/Info.plist" << EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
