84fdf9dd41743a7c60d65e951b7ef0f808be77dd
Richard Wilkes <wilkes@me.com>
2019-04-01 16:45:49 -0700

    Update for latest webapp

--- build.sh
+++ build.sh
@@ -3,65 +3,68 @@ set -eo pipefail
 
 APP_NAME=example
 APP_BUNDLE_NAME=Example
-APP_VERSION=0.2.0
-APP_VERSION_SHORT=0.2
+APP_VERSION=0.3.0
+APP_VERSION_SHORT=0.3
 BUNDLE_ID=com.trollworks.example
 COPYRIGHT_YEARS=2018-2019
 COPYRIGHT_OWNER="Richard A. Wilkes"
 
 # Setup OS_TYPE
 case $(uname -s) in
-    Darwin*)  OS_TYPE=darwin ;;
-    #Linux*)   OS_TYPE=linux ;;
-    MINGW64*) OS_TYPE=windows ;;
-    *)        echo "Unsupported OS"; false ;;
+	Darwin*)  OS_TYPE=darwin ;;
+	#Linux*)   OS_TYPE=linux ;;
+	MINGW64*) OS_TYPE=windows ;;
+	*)        echo "Unsupported OS"; false ;;
 esac
 
 # Setup GIT_VERSION
 if which git 2>&1 > /dev/null; then
-    if [ -z "$(git status --porcelain)" ]; then
-        STATE=clean
-    else
-        STATE=dirty
-    fi
-    GIT_VERSION=$(git rev-parse HEAD)-$STATE
+	if [ -z "$(git status --porcelain)" ]; then
+		STATE=clean
+	else
+		STATE=dirty
+	fi
+	GIT_VERSION=$(git rev-parse HEAD)-$STATE
 else
-    GIT_VERSION=Unknown
+	GIT_VERSION=Unknown
 fi
 
 # Ensure the build number is set to something
 if [ -z $BUILD_NUMBER ]; then
-    BUILD_NUMBER=Unknown
+	BUILD_NUMBER=Unknown
 fi
 
-# Setup CEF -- subsequent use of the installed 'cef' tool
-# requires that $GOPATH/bin be in your $PATH
-go install -v github.com/richardwilkes/cef
+# Setup tools we need
+TOOLS_DIR=$PWD/tools
+mkdir -p $TOOLS_DIR
+export PATH=$TOOLS_DIR:$PATH
+go build -o $TOOLS_DIR/cef github.com/richardwilkes/cef
 cef install
 
 # Prepare platform-specific distribution bundle
 cef dist \
-    --bundle "$APP_BUNDLE_NAME" \
-    --executable "$APP_NAME" \
-    --release "$APP_VERSION" \
-    --short-release "$APP_VERSION_SHORT" \
-    --year "$COPYRIGHT_YEARS" \
-    --owner "$COPYRIGHT_OWNER" \
-    --id $BUNDLE_ID
+	--bundle "$APP_BUNDLE_NAME" \
+	--executable "$APP_NAME" \
+	--icon AppIcon.icns \
+	--release "$APP_VERSION" \
+	--short-release "$APP_VERSION_SHORT" \
+	--year "$COPYRIGHT_YEARS" \
+	--owner "$COPYRIGHT_OWNER" \
+	--id $BUNDLE_ID
 case $OS_TYPE in
-    darwin)
-        touch "dist/macos/$APP_BUNDLE_NAME.app" # Causes Finder to refresh its state
-        TARGET_EXE="dist/macos/$APP_BUNDLE_NAME.app/Contents/MacOS/$APP_NAME"
-        ;;
-    windows)
-        TARGET_EXE="dist/windows/$APP_NAME.exe"
-        ;;
-    *)
-        echo "Unsupported OS"
-        false
-        ;;
+	darwin)
+		touch "dist/macos/$APP_BUNDLE_NAME.app" # Causes Finder to refresh its state
+		TARGET_EXE="dist/macos/$APP_BUNDLE_NAME.app/Contents/MacOS/$APP_NAME"
+		;;
+	windows)
+		TARGET_EXE="dist/windows/$APP_NAME.exe"
+		;;
+	*)
+		echo "Unsupported OS"
+		false
+		;;
 esac
 
 go build -o "$TARGET_EXE" -v \
-    -ldflags=all="-X github.com/richardwilkes/toolbox/cmdline.AppVersion=$APP_VERSION_SHORT -X github.com/richardwilkes/toolbox/cmdline.GitVersion=$GIT_VERSION -X github.com/richardwilkes/toolbox/cmdline.BuildNumber=$BUILD_NUMBER" \
-    ./main.go
+	-ldflags=all="-X github.com/richardwilkes/toolbox/cmdline.AppVersion=$APP_VERSION_SHORT -X github.com/richardwilkes/toolbox/cmdline.GitVersion=$GIT_VERSION -X github.com/richardwilkes/toolbox/cmdline.BuildNumber=$BUILD_NUMBER" \
+	./main.go
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/richardwilkes/webapp-example
 
 require (
-	github.com/richardwilkes/toolbox v1.2.0
-	github.com/richardwilkes/webapp v0.2.3
+	github.com/richardwilkes/toolbox v1.5.0
+	github.com/richardwilkes/webapp v0.3.0
 )
