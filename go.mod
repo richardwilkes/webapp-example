f0002051ad0376b81dac91a917cb0d1aadb283f2
Richard Wilkes <wilkes@me.com>
2020-01-11 12:52:02 -0800

    Update dependencies; add copyright notice; add Goland prefs

--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module github.com/richardwilkes/webapp-example
 
+go 1.13
+
 require (
-	github.com/richardwilkes/toolbox v1.9.3
-	github.com/richardwilkes/webapp v0.5.2
+	github.com/richardwilkes/toolbox v1.24.0
+	github.com/richardwilkes/webapp v0.5.3
 )
--- main.go
+++ main.go
@@ -1,3 +1,12 @@
+// Copyright ©2018-2020 by Richard A. Wilkes. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, version 2.0. If a copy of the MPL was not distributed with
+// this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// This Source Code Form is "Incompatible With Secondary Licenses", as
+// defined by the Mozilla Public License, version 2.0.
+
 package main
 
 import (
@@ -14,7 +23,7 @@ func main() {
 	cmdline.AppName = "Example"
 	cmdline.AppCmdName = "example"
 	cmdline.AppVersion = "0.2"
-	cmdline.CopyrightYears = "2018-2019"
+	cmdline.CopyrightYears = "2018-2020"
 	cmdline.CopyrightHolder = "Richard A. Wilkes"
 	cmdline.AppIdentifier = "com.trollworks.webapp.example"
 
